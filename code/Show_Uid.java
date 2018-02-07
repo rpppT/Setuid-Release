5c46cc2b1fcf85605f362719d19f6faa0ce29b86
rpppT <36104309+rpppT@users.noreply.github.com>
2018-02-08 01:53:53 +0900

    Update Show_Uid.java

--- code/Show_Uid.java
+++ code/Show_Uid.java
@@ -11,7 +11,7 @@ public class Show_Uid implements DataImple, Color{
         System.out.println(ANSI_RED+"[최근 결과로 얻어진 Setuid 파일 존재]==> "+ANSI_RESET+input.readObject());
       }
     }catch(Exception e){
-
+	 //예외가 무조건 발생하는 곳...EOFException이 무조건 발생해야지 프로그램을 복귀 시킬 수 있음.
     }
     System.out.println();
 	}

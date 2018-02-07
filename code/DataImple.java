9b4e146e1beec911c64f744e3b0b677b66b176cc
rpppT <36104309+rpppT@users.noreply.github.com>
2018-02-08 01:41:12 +0900

    Update DataImple.java

--- code/DataImple.java
+++ code/DataImple.java
@@ -5,10 +5,13 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public interface DataImple extends Serializable,Color {
-  public static final long serailVersionUID = 3518123450987612345L; 
+        public static final long serailVersionUID = 3518123450987612345L; //serialVersionUID
+	
 	public static final ArrayList<String> data_set = new ArrayList<>();
+
+	////////////////////////////////RELATIVE FILE///////////////////////////////////
 	public static final File path = new File(System.getProperty("user.home")+"/Setuid/beta_test3/log");
 	public static final File ver = new File(System.getProperty("user.home")+"/Setuid/beta_test3/temp");
-  public static final File Uid_Dir = new File(System.getProperty("user.home")+"/Setuid/beta_test3/Uid_Dir/set_file");
+  	public static final File Uid_Dir = new File(System.getProperty("user.home")+"/Setuid/beta_test3/Uid_Dir/set_file");
 }
 
